7fd077c3f4815db0b0a5fef8e7a655529027f2c5
NachoS <secaduras87@gmail.com>
2023-03-20 19:40:35 +0100

    Error

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,20 +1,10 @@
 package main.java.ieseuropa;
 
 public class Array {
-	
-	private static int saberMaximo() { 
-		int[] array = { 10, 33, 81, 53, 22 };
-		int max = array[0];
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] > max) {
-				max = array[i];
-			}
-		}
-		return max;
-	}
 
 	public static void main(String[] args) {
-		System.out.println("El máximo es " + saberMaximo());
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
 	}
 
 }
