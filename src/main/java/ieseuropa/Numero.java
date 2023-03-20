3c34e82b2dab631f240094cee8bef6eeac51995d
NachoS <secaduras87@gmail.com>
2023-03-20 18:48:11 +0100

    Añadido mostrar 15 primeros números serie Pell

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -10,10 +10,20 @@ public class Numero {
 		return resultado;
 	}
 
+	private static void pellSeries() { 
+		int pell1 = 0, pell2 = 1;
+		for (int i = 0; i < 15; i++) {
+			System.out.print(pell1 + " ");
+			int nextPell = pell2 * 2 + pell1;
+			pell1 = pell2;
+			pell2 = nextPell;
+		}
+	}
 	public static void main(String[] args) {
 		int num = 5;
 		System.out.println("El factorial de " + num + " es " + factorial(num));
-
+		System.out.print("La serie de Pell: ");
+		pellSeries();
 	}
 
 }
