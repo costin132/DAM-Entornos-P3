469119386240676bbfce357040b9aa126c562ef1
NachoS <secaduras87@gmail.com>
2023-03-20 18:52:28 +0100

    Añadido saber número máximo de un array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,21 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	private static int saberMaximo() { 
+		int[] array = { 1, 3, 8, 5, 2 };
+		int max = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		System.out.println("El máximo es " + saberMaximo());
+
 	}
 
 }
