45e0a882a5730d3cfa469b265673f6ad2ebaa9d9
Carol <106676148+Mel-Gr@users.noreply.github.com>
2023-03-22 20:33:35 +0100

    añadido metodo borrar un string de otro

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -9,10 +9,16 @@ public class Strings {
 	      System.out.print (a + " " + b + " ");
 	    }
 	}
+	
+  public static String borrarAparicion (String a, String b) {
+	String resultado = a.toLowerCase().replace(b.toLowerCase(), "");
+	return resultado;
+  }
 
   public static void main(String[] args) {
 		// TODO Auto-generated method stub
     repeticionStrings("hola", "caracola");
+    System.out.print (borrarAparicion ("Hola que tal estas?", "estas"));
   }
   
 }
