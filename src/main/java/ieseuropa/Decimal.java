26ca9e55513474ff2d2816bae187121f03885788
KIKE <KIKE@DIEZ>
2023-03-22 17:46:19 +0100

    Método para eliminar los decimales de un número.

---
+++ src/main/java/ieseuropa/Decimal.java
@@ -0,0 +1,14 @@
+package main.java.ieseuropa;
+
+public class Decimal {
+	
+	public static int eliminarDecimales(double numero) {
+		return (int) numero;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
