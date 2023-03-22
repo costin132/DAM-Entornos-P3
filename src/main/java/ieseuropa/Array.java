99c4569cd306d5d145e756a23982ee97a1e807a3
KIKE <KIKE@DIEZ>
2023-03-22 18:58:31 +0100

    Metodo para ver el minimo de un array.

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,9 +2,19 @@ package main.java.ieseuropa;
 
 public class Array {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+	public static int calcularMinimo(int[] array) {
+		int minimo = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < minimo) {
+				minimo = array[i];
+			}
+		}
+		return minimo;
 	}
 
+	public static void main(String[] args) {
+		int[] numeros = { 10, 5, 20, 3, 8 };
+		int minimo = calcularMinimo(numeros);
+		System.out.println("El mínimo es: " + minimo);
+	}
 }
