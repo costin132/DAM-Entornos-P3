1c1e6aa2824ce3f9518143b7f109f1fd2e66f228
KIKE <KIKE@DIEZ>
2023-03-22 18:35:13 +0100

    Metodo para eliminar decimales.

--- src/main/java/ieseuropa/Decimal.java
+++ src/main/java/ieseuropa/Decimal.java
@@ -7,7 +7,9 @@ public class Decimal {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float numeroDecimal = 3.1416f;
+		int numeroSinDecimales = Decimal.eliminarDecimales(numeroDecimal);
+		System.out.println("El número " + numeroDecimal + " sin decimales es: " + numeroSinDecimales);
+	
 	}
 }
