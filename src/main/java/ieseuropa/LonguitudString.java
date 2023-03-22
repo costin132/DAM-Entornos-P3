6bd00bb7b29195642fcb5d96b8097f621f3273c0
NachoS <secaduras87@gmail.com>
2023-03-22 12:06:37 +0100

    Añadido método para devolver la longuitud de la cadena

---
+++ src/main/java/ieseuropa/LonguitudString.java
@@ -0,0 +1,15 @@
+package main.java.ieseuropa;
+
+public class LonguitudString {
+
+	public static String devolverLongitud(String nombre) {
+		int longitud = nombre.length();
+		return "La longitud de la cadena es: " + longitud;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(devolverLongitud("Hola mundo"));
+
+	}
+
+}
