60147c6050b40c8eb8bdf4b9348341db3c03af99
dev <dev@debian>
2023-03-31 19:49:51 +0200

    Actualizacion array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,5 +1,7 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+
 public class Array {
 	
 	private static int saberMaximo() { 
@@ -12,10 +14,59 @@ public class Array {
 		}
 		return max;
 	}
+	public static double mediaArray(int[] datos) {
+		int cantDatos = 0;
+		double acumulador = 0L;
+		for(int i = 0; i < datos.length; i++) {
+			acumulador = acumulador + datos[i];
+			cantDatos++;
+		}
+		return acumulador/cantDatos;
+	}
+	
 
-	public static void main(String[] args) {
-		System.out.println("El máximo es " + saberMaximo());
-
+	public static double mediana (int [] a) {
+		Arrays.sort(a);
+		double mediana = 0;
+		if (a.length % 2 != 0) {
+			mediana = a[(a.length/2)];
+		}else {
+			int n = a[a.length / 2 - 1];
+			int m = a[a.length / 2];
+			mediana = (n+m) / 2;
+		}
+		return mediana;
 	}
 
+
+	public static int calcularMinimo(int[] array) {
+		int minimo = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < minimo) {
+				minimo = array[i];
+			}
+		}
+		return minimo;
+	}
+
+	public static void ordenarDeMenorAMayor(int[] array) {
+		Arrays.sort(array);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numeros = { 10, 5, 20, 3, 8 };
+		int minimo = calcularMinimo(numeros);
+		System.out.println("El mínimo es: " + minimo);
+		System.out.println("El máximo es " + saberMaximo());
+		System.out.println(mediaArray(new int[] { 3, 14, 32, 1}));
+		System.out.println(mediaArray(new int[] { 12, 25, 22, 52}));
+		System.out.println(mediaArray(new int[] { 1, 84, 23, 43}));
+    		int [] a = {2,4,8,3,1,7};
+		System.out.println(mediana(a));
+		int[] numero = { 3, 5, 1, 4, 2 };
+		System.out.println("Array antes del ordenamiento: " + Arrays.toString(numero));
+		ordenarDeMenorAMayor(numero);
+		System.out.println("Array después del ordenamiento: " + Arrays.toString(numero));
+	}
 }
